8c5718bb467cb33d4bca0550cb25c9cc5b952e29
laian <tanlaian2004@gmail.com>
2023-08-22 17:52:46 +0800

    feat: config

--- config.hpp
+++ config.hpp
@@ -11,16 +11,29 @@
 
 using std::string;
 
-class ServerConfig
+class ServerLocation
 {
 	public:
 
+	private:
+		std::map<string, std::vector<string>>	rules;
+};
+
+class ServerConfig
+{
+	public:
+		ServerConfig();
+		ServerConfig(const ServerConfig &server_config);
+		ServerConfig &operator = (const ServerConfig &server_config);
+		~ServerConfig();
 
 
 	private:
-		std::vector<string> listen;
+		std::vector<string>					listen;
+		// stuff
 
-		std::map<string, std::vector<string> >	locations;
+
+		std::map<string, ServerLocation>	locations;
 
 };
 
@@ -28,13 +41,11 @@ class Config
 {
 	public:
 		Config();
+		// Construtor to handle the whole config file
 		Config(std::string filename);
 		Config(const Config &config);
 		Config &operator = (const Config &config);
 		~Config();
-
-
-
 
 	private:
 		std::vector<ServerConfig>	servers;
